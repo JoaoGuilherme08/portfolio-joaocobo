607dea257b2ab3e860611f658245a62c6723e21e
joaoguilherme08 <joaocobo20@gmail.com>
2025-09-11 10:44:23 -0300

    first commit

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,27 +1,27 @@
 "use client";
-import { Github, Mail, Twitter } from "lucide-react";
+import { Github, Mail, Linkedin } from "lucide-react";
 import Link from "next/link";
 import { Navigation } from "../components/nav";
 import { Card } from "../components/card";
 
 const socials = [
 	{
-		icon: <Twitter size={20} />,
-		href: "https://twitter.com/chronark_",
-		label: "Twitter",
-		handle: "@chronark_",
-	},
-	{
-		icon: <Mail size={20} />,
-		href: "mailto:dev@chronark.com",
-		label: "Email",
-		handle: "dev@chronark.com",
+		icon: <Linkedin size={20} />,
+		href: "https://www.linkedin.com/in/joaoguilhermecobo/",
+		label: "LinkedIn",
+		handle: "@joaoguilhermecobo",
 	},
 	{
 		icon: <Github size={20} />,
-		href: "https://github.com/chronark",
+		href: "https://github.com/JoaoGuilherme08",
 		label: "Github",
-		handle: "chronark",
+		handle: "JoaoGuilherme08",
+	},
+	{
+		icon: <Mail size={20} />,
+		href: "mailto:joaocobo20@gmail.com",
+		label: "Email",
+		handle: "joaocobo20@gmail.com",
 	},
 ];
 
--- app/layout.tsx
+++ app/layout.tsx
@@ -6,24 +6,17 @@ import { Analytics } from "./components/analytics";
 
 export const metadata: Metadata = {
   title: {
-    default: "chronark.com",
-    template: "%s | chronark.com",
+    default: "joaocobo.com",
+    template: "%s | joaocobo.com",
   },
   description: "Co-founder of unkey.dev and founder of planetfall.io",
   openGraph: {
-    title: "chronark.com",
+    title: "joaocobo.com",
     description:
       "Co-founder of unkey.dev and founder of planetfall.io",
-    url: "https://chronark.com",
-    siteName: "chronark.com",
-    images: [
-      {
-        url: "https://chronark.com/og.png",
-        width: 1920,
-        height: 1080,
-      },
-    ],
-    locale: "en-US",
+    url: "https://joaocobo.com",
+    siteName: "joaocobo.com",
+    locale: "pt-BR",
     type: "website",
   },
   robots: {
@@ -36,10 +29,6 @@ export const metadata: Metadata = {
       "max-image-preview": "large",
       "max-snippet": -1,
     },
-  },
-  twitter: {
-    title: "Chronark",
-    card: "summary_large_image",
   },
   icons: {
     shortcut: "/favicon.png",
